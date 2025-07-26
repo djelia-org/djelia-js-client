7813907826ca52f568009d21c57f6d0d825e8317
sudoping01 <sudoping01@gmail.com>
2025-07-26 11:27:19 +0000

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,6 +55,33 @@
 // };
 
 
+// module.exports = {
+//     env: {
+//         browser: false,
+//         es2021: true,
+//         node: true,
+//         jest: true,
+//     },
+//     extends: [
+//         'eslint:recommended',
+//     ],
+//     parserOptions: {
+//         ecmaVersion: 'latest',
+//         sourceType: 'commonjs',
+//     },
+//     rules: {
+//         'indent': ['error', 4],
+//         'linebreak-style': ['error', 'unix'],
+//         'quotes': ['error', 'single'],
+//         'semi': ['error', 'always'],
+//         'no-unused-vars': ['warn', { 'argsIgnorePattern': '^_' }],  
+//         'no-console': 'off',
+//         'no-empty': ['error', { 'allowEmptyCatch': true }],         
+//     },
+// };
+
+
+
 module.exports = {
     env: {
         browser: false,
@@ -78,4 +105,12 @@ module.exports = {
         'no-console': 'off',
         'no-empty': ['error', { 'allowEmptyCatch': true }],         
     },
+    overrides: [
+        {
+            files: ['tests/**/*.js', 'examples/**/*.js'],
+            rules: {
+                'no-unused-vars': 'off',  
+            }
+        }
+    ]
 };
